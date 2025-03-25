29169e56e0a07b5f70fb6edf9d74fe43feeb44cd
DeanTakle <dean.takle190100@hotmail.co.uk>
2025-03-25 19:04:16 +0000

    New Naming Convention for Each Lab + Lab 2

---
+++ Lab 2/Automated.py
@@ -0,0 +1,65 @@
+from netmiko import ConnectHandler
+
+# As no ip address and SSH has been setup this has to be manually done, however once it has been configured you can use this script without issue.
+
+S1 = {
+    'device_type': 'cisco_ios',
+    'host': '192.168.1.253',
+    'username': '',
+    'password': '',
+}
+
+S2 = {
+    'device_type': 'cisco_ios',
+    'host': '192.168.1.254',
+    'username': '',
+    'password': '',
+}
+
+devices = [S1, S2]
+
+for device in devices:
+    connection = ConnectHandler(**device)
+
+    if device == S1:
+
+        connection.send_command('host S1')
+        connection.send_config_set(
+            'line con 0', 'password cisco', 'login', 'exit')
+        connection.send_command('enable secret class')
+        connection.send_command(
+            'banner motd # Authorized access only. Violators will be prosecuted to the full extent of the law. #')
+
+        connection.send_config_set(
+            'int vlan1', 'ip add 192.168.1.253 255.255.255.0', 'no shut')
+
+        check_config = connection.send_command('do show run')
+        print(check_config)
+
+        connection.send_command('do copy run start')
+
+        connection.disconnect()
+
+    elif device == S2:
+
+        connection.send_command('host S2')
+        connection.send_config_set(
+            'line con 0', 'password cisco', 'login', 'exit')
+        connection.send_command('enable secret class')
+        connection.send_command(
+            'banner motd # Authorized access only. Violators will be prosecuted to the full extent of the law. #')
+
+        connection.send_config_set(
+            'int vlan1', 'ip add 192.168.1.254', 'no shut')
+
+        check_config = connection.send_command('do show run')
+        print(check_config)
+
+        connection.send_command('do copy run start')
+
+        connection.disconnect()
+
+    else:
+        print('No device found')
+
+print('Configuration completed')
